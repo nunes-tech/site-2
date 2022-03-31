214c825fbf4068aa0e4d906b8c93543ef172d1cb
Francisco Nunes <92335032+nunes-tech@users.noreply.github.com>
2022-03-31 15:15:55 -0300

    atualizado menu

--- meuscript.js
+++ meuscript.js
@@ -1,6 +1,11 @@
-var botaoMenu = document.querySelector('.btn-menu');
-var menu = document.querySelector('.menu');
+let botaoMenu = document.querySelector('.btn-menu');
+let menu = document.querySelector('.menu');
 botaoMenu.addEventListener('click', mostrarMenu);
 function mostrarMenu(){
     menu.classList.toggle('ativo');
+    if(menu.classList.contains("ativo")){
+        botaoMenu.innerHTML = ("Menu ✖")
+    } else {
+        botaoMenu.innerHTML = ("Menu ☰")
+    }
 }
